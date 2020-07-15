ea5cd848bc88d2161f8eb9436a0874638856afce
unknown <adeebabbas@outlook.com>
2020-07-15 15:28:58 -0400

    simple

--- parallelized.py
+++ parallelized.py
@@ -15,32 +15,26 @@ combinations_generator = itertools.product(string.ascii_lowercase, repeat=2) #re
 combinations = list(map(''.join, combinations_generator))
 f = open("student_pwd.txt", 'r')
 
-
 adeeb_pass = f.readlines()[0].split(',')[1]
-# friend_1 = f.readlines()[1].split(',')[1]
-# friend_2 = f.readlines()[2].split(',')[1]
-
-def run(k, salt):
-    passwd = combinations[k]
-    hashed = bp.hashpw(passwd.encode(), salt)
-    if bp.checkpw(hashed, adeeb_pass.encode()):
+# print(adeeb_pass)
+# def run(k):
+for passwd in combinations:
+    # passwd = combinations[k]
+    if bp.checkpw(passwd.encode(), adeeb_pass.encode()) is True:
         print("Password Matches")
-        print(passwd.decode())
-        sys.exit()
+        print(passwd)
+# print(combinations)
 
-def main():
-    pool = multiprocessing.Pool(os.cpu_count() - 1)
-    length = range(len(combinations))
-    start_time = time.time()
-    while True:
-        salt = bp.gensalt()
-        runner = partial(run, salt=salt)
-        for _ in tqdm.tqdm(pool.imap_unordered(runner, length), total=len(combinations)):
-            pass
-    end = time.time()
-    print((end-start_time)/60)
-    pool.close()
-    pool.join()
+# def main():
+#     pool = multiprocessing.Pool(os.cpu_count() - 1)
+#     length = range(len(combinations))
+#     start_time = time.time()
+#     for _ in tqdm.tqdm(pool.imap_unordered(run, length), total=len(combinations)):
+#         pass
+#     end = time.time()
+#     print((end-start_time)/60)
+#     pool.close()
+#     pool.join()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
