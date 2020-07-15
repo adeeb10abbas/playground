78d14ef2b493e8f1237b0ab52c82dc70e7e2be3c
unknown <adeebabbas@outlook.com>
2020-07-15 12:47:55 -0400

    updates

--- parallelized.py
+++ parallelized.py
@@ -8,6 +8,7 @@ import sys
 import os
 import warnings
 warnings.simplefilter(action='ignore')
+from functools import partial
 
 
 combinations_generator = itertools.product(string.ascii_lowercase, repeat=2) #repeat=2
@@ -19,22 +20,25 @@ adeeb_pass = f.readlines()[0].split(',')[1]
 # friend_1 = f.readlines()[1].split(',')[1]
 # friend_2 = f.readlines()[2].split(',')[1]
 
-def run(k):
+def run(k, salt):
     passwd = combinations[k]
-    hashed = bp.hashpw(passwd.encode(), bp.gensalt())
+    hashed = bp.hashpw(passwd.encode(), salt)
     if bp.checkpw(hashed, adeeb_pass.encode()):
         print("Password Matches")
         print(passwd.decode())
         sys.exit()
 
-
 def main():
-    pool = multiprocessing.Pool(os.cpu_count())
+    pool = multiprocessing.Pool(6)
     length = range(len(combinations))
     start_time = time.time()
-    for _ in tqdm.tqdm(pool.imap_unordered(run, length), total=len(combinations)):
-        pass
+    while True:
+        salt = bp.gensalt(12)
+        runner = partial(run, salt=salt)
+        for _ in tqdm.tqdm(pool.imap_unordered(runner, length), total=len(combinations)):
+            pass
     end = time.time()
+    print((end-start_time)/60)
     pool.close()
     pool.join()
 
